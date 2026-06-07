68c97fa6f40f063f88081bf7b422f0cd3ae1b256
Himadri Saha <Himadri_Saha@student.uml.edu>
2026-06-07 13:06:11 -0400

    finished req5 (be sure to add this to report )

--- labs/lab_1/src/req5.ino
+++ labs/lab_1/src/req5.ino
@@ -9,5 +9,10 @@
 */
 
 void req5() {
-  
+  Serial.println("Model Array: Maps existing g_model byte array to a useable tflite::Model* object");
+  Serial.println("AllocateTensors(): Reserves space inside tensor_arena for the model's input/output/intermediate tensors and reports whether they fit via allocate_status.");
+  Serial.println("Invoke(): Executes the model's forward computation on the current input tensor and writes its prediction into output, checking invoke_status for errors.");
+  Serial.println("Output post-processing: Dequantizes output's int8 value into a float y via its scale/zero_point, then hands (x, y) off to HandleOutput()"):
+
+  delay(10000)
 }
