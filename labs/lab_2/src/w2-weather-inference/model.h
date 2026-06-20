1099103dd92b0fd9c6d6d706df63c8bf2cdc2f03
Himadri Saha <Himadri_Saha@student.uml.edu>
2026-06-20 19:05:31 -0400

    organization

---
+++ labs/lab_2/src/w2-weather-inference/model.h
@@ -0,0 +1,11 @@
+// Lab 2 - Track W - model.h
+// Declares the int8 weather model exported from weather_training.ipynb.
+// Defined in model.cpp (g_model / g_model_len).
+
+#ifndef LAB2_TRACK_W_MODEL_H_
+#define LAB2_TRACK_W_MODEL_H_
+
+extern const unsigned char g_model[];
+extern const int g_model_len;
+
+#endif  // LAB2_TRACK_W_MODEL_H_
